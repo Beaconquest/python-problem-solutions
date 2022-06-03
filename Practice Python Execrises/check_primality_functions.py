9955937e31a48aeb54ad75346c345a66902406d3
Promise Mwenga <promise_mwenga@hotmail.com>
2022-06-03 19:09:11 -0300

    updated execrise 11

--- Practice Python Execrises/check_primality_functions.py
+++ Practice Python Execrises/check_primality_functions.py
@@ -2,3 +2,20 @@
 
 # Execrise 11
 
+user_number = int(input("Enter a number:"))
+
+def is_prime(number):
+    """A function to determine whether a number is a prime Number."""
+    a = [i for i in range(2, number + 1) if number % i == 0]
+
+    if number == 1:
+        print(f"{number} is Prime.")
+        return a
+    elif len(a) == 1:
+        print(f"{number} is Prime.")
+        return a
+    else:
+        print(f"{number} is not Prime.")
+        return a
+
+print(is_prime(user_number))
