361e73a9fe728ed9b4ecdd9d34b7eb36fb8151ec
Promise Mwenga <promise_mwenga@hotmail.com>
2022-06-03 21:48:17 -0300

    update to execrise 12

--- Practice Python Execrises/list_ends.py
+++ Practice Python Execrises/list_ends.py
@@ -1,3 +1,22 @@
 # python execrises from www.practicepython.org
 
 # Execrise 7
+
+a = [5, 10, 15, 20, 25]
+
+def first_and_last(a_list):
+    b = []
+    length = len(a_list)
+    
+    # first attempt
+    # b.append(a_list[0])
+    # b.append(a_list[length -1])
+    
+    
+    # second attempt-- works but its a reach, but its different
+    d =list(enumerate(a_list))
+    c = [d[0][1], d[length -1][1]]
+
+    return c
+
+print(first_and_last(a))
